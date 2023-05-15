b4481b089dc92f873a5802d6f4f56ebce4cf613c
jingdor <admin@jingdor.com>
2023-05-15 08:48:02 +0800

    增加clusterMasterSlave测试

--- playground/sharding/client.py
+++ playground/sharding/client.py
@@ -4,5 +4,7 @@ import redis
 cli=redis.cluster.RedisCluster("127.0.0.1",6379)
 
 print(cli.set("aa","test"))
+print(cli.set("ab","test"))
+print(cli.set("ac","test"))
 print(cli.get("aa"))
 
--- playground/sharding/main.py
+++ playground/sharding/main.py
@@ -9,13 +9,35 @@ def startMaster2():
 def startMaster3():
     os.system("redis-server redis_master_3/default.conf")
 
-m1=Process(target=startMaster1)
-m1.start()
-m2=Process(target=startMaster2)
-m2.start()
-m3=Process(target=startMaster3)
-m3.start()
+def startSlave1():
+    os.system("redis-server redis_slave_1/default.conf")
+def startSlave2():
+    os.system("redis-server redis_slave_2/default.conf")
+def startSlave3():
+    os.system("redis-server redis_slave_3/default.conf")    
 
-m1.join()
-m2.join()
-m3.join()
+def main():
+    m1=Process(target=startMaster1)
+    m2=Process(target=startMaster2)
+    m3=Process(target=startMaster3)
+    s1=Process(target=startSlave1)
+    s2=Process(target=startSlave2)
+    s3=Process(target=startSlave3)
+   
+    m1.start()
+    m2.start()
+    m3.start()
+    s1.start()
+    s2.start()
+    s3.start()
+    m1.join()
+    m2.join()
+    m3.join()
+    s1.join()
+    s2.join()
+    s3.join()
+
+
+if __name__ == '__main__':
+        main()
+
